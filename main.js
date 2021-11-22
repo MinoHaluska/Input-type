950925c21b361eec4af34b4be2eb5b5d65dc0a02
MinoHaluska <89128227+MinoHaluska@users.noreply.github.com>
2021-11-22 07:47:58 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,261 @@
+'use strict'
+
+const email = document.getElementById('email');
+
+email.addEventListener('click',()=>{
+    email.value = '';
+})
+
+const h1 = document.getElementById('input-types');
+
+anime({
+    targets: h1,
+    lineHeight: '10vh',
+    easing: 'easeInOutQuad',
+    delay: 1000,
+    duration: 2000
+})
+
+
+const text = document.querySelector('.text');
+text.style.transform = 'translateX(-600px)';
+anime({
+    targets: text,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 2500,
+    duration: 1000
+})
+
+
+const password = document.querySelector('.password');
+password.style.transform = 'translateX(-600px)';
+anime({
+    targets: password,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 2600,
+    duration: 1000
+})
+
+const radio = document.querySelector('.radio');
+radio.style.transform = 'translateX(-600px)';
+anime({
+    targets: radio,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 2700,
+    duration: 1000
+})
+
+const checkbox = document.querySelector('.checkbox');
+checkbox.style.transform = 'translateX(-600px)';
+anime({
+    targets: checkbox,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 2800,
+    duration: 1000
+})
+
+const button = document.querySelector('.button');
+button.style.transform = 'translateX(-600px)';
+anime({
+    targets: button,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 2900,
+    duration: 1000
+})
+
+const color = document.querySelector('.color');
+color.style.transform = 'translateX(-600px)';
+anime({
+    targets: color,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 2900,
+    duration: 1000
+})
+
+
+const email2 = document.querySelector('.email2');
+email2.style.transform = 'translateX(-600px)';
+anime({
+    targets: email2,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 3000,
+    duration: 1000
+})
+
+const file = document.querySelector('.file');
+file.style.transform = 'translateX(-600px)';
+anime({
+    targets: file,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 3100,
+    duration: 1000
+})
+
+const hidden = document.querySelector('.hidden');
+hidden.style.transform = 'translateX(-600px)';
+anime({
+    targets: hidden,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 3200,
+    duration: 1000
+})
+
+const image = document.querySelector('.image');
+image.style.transform = 'translateX(-600px)';
+anime({
+    targets: image,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 3300,
+    duration: 1000
+})
+
+const number = document.querySelector('.number');
+number.style.transform = 'translateX(-600px)';
+anime({
+    targets: number,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 3400,
+    duration: 1000
+})
+
+const range = document.querySelector('.range');
+range.style.transform = 'translateX(-600px)';
+anime({
+    targets: range,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 3500,
+    duration: 1000
+})
+
+const search = document.querySelector('.search');
+search.style.transform = 'translateX(-600px)';
+anime({
+    targets: search,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 3600,
+    duration: 1000
+})
+
+const tel = document.querySelector('.tel');
+tel.style.transform = 'translateX(-600px)';
+anime({
+    targets: tel,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 3700,
+    duration: 1000
+})
+
+const time = document.querySelector('.time');
+time.style.transform = 'translateX(-600px)';
+anime({
+    targets: time,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 3800,
+    duration: 1000
+})
+
+const date = document.querySelector('.date');
+date.style.transform = 'translateX(-600px)';
+anime({
+    targets: date,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 3900,
+    duration: 1000
+})
+
+const datetime = document.querySelector('.datetime-local');
+datetime.style.transform = 'translateX(-600px)';
+anime({
+    targets: datetime,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 4000,
+    duration: 1000
+})
+
+const week = document.querySelector('.week');
+week.style.transform = 'translateX(-600px)';
+anime({
+    targets: week,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 4100,
+    duration: 1000
+})
+
+const month = document.querySelector('.month');
+month.style.transform = 'translateX(-600px)';
+anime({
+    targets: month,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 4200,
+    duration: 1000
+})
+
+const url = document.querySelector('.url');
+url.style.transform = 'translateX(-600px)';
+anime({
+    targets: url,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 4300,
+    duration: 1000
+})
+
+const submit = document.querySelector('.submit');
+submit.style.transform = 'translateX(-600px)';
+anime({
+    targets: submit,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 4400,
+    duration: 1000
+})
+
+const reset = document.querySelector('.reset');
+reset.style.transform = 'translateX(-600px)';
+anime({
+    targets: reset,
+    translateX: 0,
+    easing: 'easeInOutQuad',
+    delay: 4500,
+    duration: 1000
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
